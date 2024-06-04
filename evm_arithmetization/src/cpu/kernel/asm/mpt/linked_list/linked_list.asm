77a46c269ad9c1679e9581128ce4c657cfae7039
Linda Guiga <lindaguiga3@gmail.com>
2024-06-04 11:40:02 +0800

    Update journaling

--- evm_arithmetization/src/cpu/kernel/asm/mpt/linked_list/linked_list.asm
+++ evm_arithmetization/src/cpu/kernel/asm/mpt/linked_list/linked_list.asm
@@ -219,7 +219,7 @@ insert_new_account:
 %endmacro
 
 
-/// Search the account addr andin the linked list.
+/// Search the account addr in the linked list.
 /// Return `1, payload_ptr` if the account was not found, `1, original_ptr` if it was already present
 /// and this is the first access, or `0, original_ptr` if it was already present and accessed.
 global search_account:
@@ -294,6 +294,11 @@ global remove_account:
     POP
     JUMP
 
+%macro remove_account
+    %stack (addr) -> (addr, %%after)
+    %jump(remove_account)
+%%after:
+%endmacro
 
 //
 //
