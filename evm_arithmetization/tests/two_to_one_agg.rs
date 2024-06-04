65f5cf371146c674ad0ebba814fc7ea362c4ef9d
Einar Rasmussen <erasmussen@polygon.technology>
2024-06-05 02:03:43 +0800

    WIP: test: can generate proofs of unrelated blocks

--- evm_arithmetization/tests/two_to_one_agg.rs
+++ evm_arithmetization/tests/two_to_one_agg.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 
+use env_logger::fmt::Timestamp;
 use env_logger::{try_init_from_env, Env, DEFAULT_FILTER_ENV};
 use ethereum_types::{Address, BigEndianHash, H256, U256};
 use evm_arithmetization::generation::mpt::{AccountRlp, LegacyReceiptRlp};
@@ -13,7 +14,9 @@ use mpt_trie::nibbles::Nibbles;
 use mpt_trie::partial_trie::{HashedPartialTrie, PartialTrie};
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::plonk::config::PoseidonGoldilocksConfig;
+use plonky2::plonk::proof::ProofWithPublicInputs;
 use plonky2::util::timing::TimingTree;
+use starky::stark::Stark;
 
 type F = GoldilocksField;
 const D: usize = 2;
@@ -174,6 +177,145 @@ fn dummy_inputs(inputs: &GenerationInputs) -> GenerationInputs {
     }
 }
 
+
+/// Get `GenerationInputs` for a simple token transfer txn, where the block has
+/// the given timestamp.
+fn empty_transfer(timestamp: u64) -> anyhow::Result<GenerationInputs> {
+    init_logger();
+
+    let beneficiary = hex!("deadbeefdeadbeefdeadbeefdeadbeefdeadbeef");
+    let sender = hex!("2c7536e3605d9c16a7a3d7b1898e529396a65c23");
+    let to = hex!("a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0");
+
+    let sender_state_key = keccak(sender);
+    let to_state_key = keccak(to);
+
+    let sender_nibbles = Nibbles::from_bytes_be(sender_state_key.as_bytes()).unwrap();
+    let to_nibbles = Nibbles::from_bytes_be(to_state_key.as_bytes()).unwrap();
+
+    let sender_account_before = AccountRlp {
+        nonce: 5.into(),
+        balance: eth_to_wei(100_000.into()),
+        storage_root: HashedPartialTrie::from(Node::Empty).hash(),
+        code_hash: keccak([]),
+    };
+    let to_account_before = AccountRlp::default();
+
+    let state_trie_before: HashedPartialTrie = Node::Leaf {
+        nibbles: sender_nibbles,
+        value: rlp::encode(&sender_account_before).to_vec(),
+    }
+    .into();
+    let checkpoint_state_trie_root = state_trie_before.hash();
+    assert_eq!(checkpoint_state_trie_root, hex!("ef46022eafbc33d70e6ea9c6aef1074c1ff7ad36417ffbc64307ad3a8c274b75").into());
+
+    let tries_before = TrieInputs {
+        state_trie: HashedPartialTrie::from(Node::Empty),
+        transactions_trie: HashedPartialTrie::from(Node::Empty),
+        receipts_trie: HashedPartialTrie::from(Node::Empty),
+        storage_tries: vec![],
+    };
+
+    // Generated using a little py-evm script.
+    let txn = hex!("f861050a8255f094a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0648242421ba02c89eb757d9deeb1f5b3859a9d4d679951ef610ac47ad4608dc142beb1b7e313a05af7e9fbab825455d36c36c7f4cfcafbeafa9a77bdff936b52afb36d4fe4bcdd");
+    let value = U256::from(100u32);
+
+    let block_metadata = BlockMetadata {
+        block_beneficiary: Address::from(beneficiary),
+        block_timestamp: timestamp.into(),
+        block_number: 1.into(),
+        block_difficulty: 0x020000.into(),
+        block_random: H256::from_uint(&0x020000.into()),
+        block_gaslimit: 0xff112233u32.into(),
+        block_chain_id: 1.into(),
+        block_base_fee: 0xa.into(),
+        block_gas_used: 0.into(),
+        block_bloom: [0.into(); 8],
+    };
+
+    let mut contract_code = HashMap::new();
+    //contract_code.insert(keccak(vec![]), vec![]);
+
+    let expected_state_trie_after: HashedPartialTrie = {
+        let txdata_gas = 2 * 16;
+        let gas_used = 21_000 + txdata_gas;
+
+        let sender_account_after = AccountRlp {
+            balance: sender_account_before.balance - value - gas_used * 10,
+            nonce: sender_account_before.nonce + 1,
+            ..sender_account_before
+        };
+        let to_account_after = AccountRlp {
+            balance: value,
+            ..to_account_before
+        };
+
+        let mut children = core::array::from_fn(|_| Node::Empty.into());
+        children[sender_nibbles.get_nibble(0) as usize] = Node::Leaf {
+            nibbles: sender_nibbles.truncate_n_nibbles_front(1),
+            value: rlp::encode(&sender_account_after).to_vec(),
+        }
+        .into();
+        children[to_nibbles.get_nibble(0) as usize] = Node::Leaf {
+            nibbles: to_nibbles.truncate_n_nibbles_front(1),
+            value: rlp::encode(&to_account_after).to_vec(),
+        }
+        .into();
+        Node::Branch {
+            children,
+            value: vec![],
+        }
+        .into()
+    };
+
+    let receipt_0 = LegacyReceiptRlp {
+        status: true,
+        cum_gas_used: 21032.into(),
+        bloom: vec![0; 256].into(),
+        logs: vec![],
+    };
+    let mut receipts_trie = HashedPartialTrie::from(Node::Empty);
+    receipts_trie.insert(
+        Nibbles::from_str("0x80").unwrap(),
+        rlp::encode(&receipt_0).to_vec(),
+    )?;
+    let transactions_trie: HashedPartialTrie = Node::Leaf {
+        nibbles: Nibbles::from_str("0x80").unwrap(),
+        value: txn.to_vec(),
+    }
+    .into();
+
+    let trie_roots_after = TrieRoots {
+        state_root: expected_state_trie_after.hash(),
+        transactions_root: transactions_trie.hash(),
+        receipts_root: receipts_trie.hash(),
+    };
+
+    let tries_after = TrieRoots {
+        state_root: tries_before.state_trie.hash(),
+        transactions_root: tries_before.transactions_trie.hash(),
+        receipts_root: tries_before.receipts_trie.hash(),
+    };
+    let inputs = GenerationInputs {
+        signed_txn: None,
+        withdrawals: vec![],
+        tries: tries_before.clone(),
+        trie_roots_after: tries_after,
+        contract_code,
+        checkpoint_state_trie_root: tries_before.state_trie.hash(),
+        block_metadata,
+        txn_number_before: 0.into(),
+        gas_used_before: 0.into(),
+        gas_used_after: 0.into(),
+        block_hashes: BlockHashes {
+            prev_hashes: vec![H256::default(); 256],
+            cur_hash: H256::default(),
+        },
+    };
+
+    Ok(inputs)
+}
+
 #[test]
 #[ignore]
 fn test_two_to_one_aggregation() -> anyhow::Result<()> {
@@ -227,55 +369,41 @@ fn test_two_to_one_aggregation() -> anyhow::Result<()> {
     all_circuits.verify_two_to_one_aggregation(&proof)
 }
 
-#[test]
-/// Run: RUSTFLAGS=-Ctarget-cpu=native cargo test --release -- three_to_one
-/// Aggregate a sequential proof containing three proofs with the structure `((A,B),C)`.
-/// We take the previous example and extend it.
-///
-///
-///  A    B   C
-///   \  /   /
-///   (A,B) /
-///     \  /
-///   ((A,B),C)
-///
-fn test_three_to_one_block_aggregation_seq() -> anyhow::Result<()> {
-    let all_stark = AllStark::<F, D>::default();
-    let config = StarkConfig::standard_fast_config();
 
-    let inputs0 = simple_transfer(4)?;
-    let dummy0 = dummy_inputs(&inputs0);
-    let inputs1 = simple_transfer(1)?;
-    let dummy1 = dummy_inputs(&inputs1);
-    let inputs2 = simple_transfer(3)?;
-    let dummy2 = dummy_inputs(&inputs2);
+fn generate_test_block_proof(timestamp: u64, timing: &mut TimingTree, all_circuits: &AllRecursiveCircuits<GoldilocksField, PoseidonGoldilocksConfig, 2>, all_stark: &AllStark<GoldilocksField, 2>, config: StarkConfig) -> anyhow::Result<ProofWithPublicInputs<GoldilocksField, PoseidonGoldilocksConfig, 2>> {
+    let inputs0 = empty_transfer(timestamp)?;
+    let inputs = inputs0.clone();
+    let dummy0 =
+    GenerationInputs {
+        txn_number_before: inputs.txn_number_before ,
+        gas_used_before: inputs.gas_used_after,
+        gas_used_after: inputs.gas_used_after,
+        signed_txn: None,
+        withdrawals: vec![],
+        tries: TrieInputs {
+            state_trie: HashedPartialTrie::from(Node::Hash(inputs.trie_roots_after.state_root)),
+            transactions_trie: HashedPartialTrie::from(Node::Hash(
+                inputs.trie_roots_after.transactions_root,
+            )),
+            receipts_trie: HashedPartialTrie::from(Node::Hash(
+                inputs.trie_roots_after.receipts_root,
+            )),
+            storage_tries: vec![],
+        },
+        trie_roots_after: inputs.trie_roots_after,
+        checkpoint_state_trie_root: inputs.checkpoint_state_trie_root,
+        contract_code: Default::default(),
+        block_metadata: inputs.block_metadata.clone(),
+        block_hashes: inputs.block_hashes.clone(),
+    };
 
-    // Preprocess all circuits.
-    let all_circuits = AllRecursiveCircuits::<F, C, D>::new(
-        &all_stark,
-        &[16..17, 9..15, 12..18, 14..15, 9..10, 12..13, 17..20],
-        &config,
-    );
 
-    let mut timing = TimingTree::new("prove root first", log::Level::Info);
     let (root_proof0, pv0) =
-        all_circuits.prove_root(&all_stark, &config, inputs0, &mut timing, None)?;
+        all_circuits.prove_root(&all_stark, &config, inputs0, timing, None)?;
     all_circuits.verify_root(root_proof0.clone())?;
     let (dummy_proof0, dummy_pv0) =
-        all_circuits.prove_root(&all_stark, &config, dummy0, &mut timing, None)?;
+        all_circuits.prove_root(&all_stark, &config, dummy0, timing, None)?;
     all_circuits.verify_root(dummy_proof0.clone())?;
-    let (root_proof1, pv1) =
-        all_circuits.prove_root(&all_stark, &config, inputs1, &mut timing, None)?;
-    all_circuits.verify_root(root_proof1.clone())?;
-    let (dummy_proof1, dummy_pv1) =
-        all_circuits.prove_root(&all_stark, &config, dummy1, &mut timing, None)?;
-    all_circuits.verify_root(dummy_proof1.clone())?;
-    let (root_proof2, pv2) =
-        all_circuits.prove_root(&all_stark, &config, inputs2, &mut timing, None)?;
-    all_circuits.verify_root(root_proof2.clone())?;
-    let (dummy_proof2, dummy_pv2) =
-        all_circuits.prove_root(&all_stark, &config, dummy2, &mut timing, None)?;
-    all_circuits.verify_root(dummy_proof2.clone())?;
 
     let (agg_proof0, pv0) = all_circuits.prove_aggregation(
         false,
@@ -285,62 +413,182 @@ fn test_three_to_one_block_aggregation_seq() -> anyhow::Result<()> {
         &dummy_proof0,
         dummy_pv0,
     )?;
-    let (agg_proof1, pv1) = all_circuits.prove_aggregation(
-        false,
-        &root_proof1,
-        pv1,
-        false,
-        &dummy_proof1,
-        dummy_pv1,
-    )?;
-    let (agg_proof2, pv2) = all_circuits.prove_aggregation(
-        false,
-        &root_proof2,
-        pv2,
-        false,
-        &dummy_proof2,
-        dummy_pv2,
-    )?;
 
     all_circuits.verify_aggregation(&agg_proof0)?;
-    all_circuits.verify_aggregation(&agg_proof1)?;
-    all_circuits.verify_aggregation(&agg_proof2)?;
 
-    // Test retrieved public values from the proof public inputs.
     let retrieved_public_values0 = PublicValues::from_public_inputs(&agg_proof0.public_inputs);
     assert_eq!(retrieved_public_values0,pv0);
-    let retrieved_public_values1 = PublicValues::from_public_inputs(&agg_proof1.public_inputs);
-    assert_eq!(retrieved_public_values1,pv1);
-    let retrieved_public_values2 = PublicValues::from_public_inputs(&agg_proof2.public_inputs);
-    assert_eq!(retrieved_public_values2,pv2);
 
     let (block_proof0, _block_public_values) = all_circuits.prove_block(
         None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
         &agg_proof0,
         pv0.clone(),
     )?;
-    let (block_proof1, _block_public_values) = all_circuits.prove_block(
-        None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
-        &agg_proof0,
-        pv1.clone(),
-    )?;
-    let (block_proof2, _block_public_values) = all_circuits.prove_block(
-        None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
-        &agg_proof0,
-        pv2.clone(),
-    )?;
-    all_circuits.verify_block(&block_proof0)?;
-    all_circuits.verify_block(&block_proof1)?;
-    all_circuits.verify_block(&block_proof2)?;
 
+    PublicValues::from_public_inputs(&block_proof0.public_inputs);
+
+    Ok(block_proof0)
+}
+
+
+#[test]
+/// Run:  RUST_BACKTRACE=1 RUSTFLAGS="-Ctarget-cpu=native -g"  cargo test --release -- --nocapture three_to_one
+/// Aggregate a sequential proof containing three proofs with the structure `((A,B),C)`.
+/// We take the previous example and extend it.
+///
+///  A    B   C
+///   \  /   /
+///   (A,B) /
+///     \  /
+///   ((A,B),C)
+///
+fn test_three_to_one_block_aggregation_cyclic() -> anyhow::Result<()> {
+    init_logger();
+    let all_stark = AllStark::<F, D>::default();
+    let config = StarkConfig::standard_fast_config();
+
+    let inputs0 = empty_transfer(4)?;
+    let inputs = inputs0.clone();
+    let dummy0 =
+    GenerationInputs {
+        txn_number_before: inputs.txn_number_before ,
+        gas_used_before: inputs.gas_used_after,
+        gas_used_after: inputs.gas_used_after,
+        signed_txn: None,
+        withdrawals: vec![],
+        tries: TrieInputs {
+            state_trie: HashedPartialTrie::from(Node::Hash(inputs.trie_roots_after.state_root)),
+            transactions_trie: HashedPartialTrie::from(Node::Hash(
+                inputs.trie_roots_after.transactions_root,
+            )),
+            receipts_trie: HashedPartialTrie::from(Node::Hash(
+                inputs.trie_roots_after.receipts_root,
+            )),
+            storage_tries: vec![],
+        },
+        trie_roots_after: inputs.trie_roots_after,
+        checkpoint_state_trie_root: inputs.checkpoint_state_trie_root,
+        contract_code: Default::default(),
+        block_metadata: inputs.block_metadata.clone(),
+        block_hashes: inputs.block_hashes.clone(),
+    };
+
+    // let inputs1 = simple_transfer(1)?;
+    // let dummy1 = dummy_inputs(&inputs1);
+    // let inputs2 = simple_transfer(3)?;
+    // let dummy2 = dummy_inputs(&inputs2);
+
+    log::info!("Stage 1");
+
+    // Preprocess all circuits.
+    let all_circuits = AllRecursiveCircuits::<F, C, D>::new(
+        &all_stark,
+        &[16..17, 9..15, 12..18, 14..15, 9..10, 12..13, 17..20],
+        &config,
+    );
+    log::info!("Stage 2");
+
+    let mut timing = TimingTree::new("prove root first", log::Level::Info);
+    // let (root_proof0, pv0) =
+    //     all_circuits.prove_root(&all_stark, &config, inputs0, &mut timing, None)?;
+    // all_circuits.verify_root(root_proof0.clone())?;
+    // let (dummy_proof0, dummy_pv0) =
+    //     all_circuits.prove_root(&all_stark, &config, dummy0, &mut timing, None)?;
+    // all_circuits.verify_root(dummy_proof0.clone())?;
+    // let (root_proof1, pv1) =
+    //     all_circuits.prove_root(&all_stark, &config, inputs1, &mut timing, None)?;
+    // all_circuits.verify_root(root_proof1.clone())?;
+    // let (dummy_proof1, dummy_pv1) =
+    //     all_circuits.prove_root(&all_stark, &config, dummy1, &mut timing, None)?;
+    // all_circuits.verify_root(dummy_proof1.clone())?;
+    // let (root_proof2, pv2) =
+    //     all_circuits.prove_root(&all_stark, &config, inputs2, &mut timing, None)?;
+    // all_circuits.verify_root(root_proof2.clone())?;
+    // let (dummy_proof2, dummy_pv2) =
+    //     all_circuits.prove_root(&all_stark, &config, dummy2, &mut timing, None)?;
+    // all_circuits.verify_root(dummy_proof2.clone())?;
+
+    log::info!("Stage 3");
+
+    // let (agg_proof0, pv0a) = all_circuits.prove_aggregation(
+    //     false,
+    //     &root_proof0,
+    //     pv0.clone(),
+    //     false,
+    //     &dummy_proof0,
+    //     dummy_pv0,
+    // )?;
+    // let (agg_proof1, pv1) = all_circuits.prove_aggregation(
+    //     false,
+    //     &root_proof1,
+    //     pv1,
+    //     false,
+    //     &dummy_proof1,
+    //     dummy_pv1,
+    // )?;
+    // let (agg_proof2, pv2) = all_circuits.prove_aggregation(
+    //     false,
+    //     &root_proof2,
+    //     pv2,
+    //     false,
+    //     &dummy_proof2,
+    //     dummy_pv2,
+    // )?;
+
+    log::info!("Stage 4");
+
+    // all_circuits.verify_aggregation(&agg_proof0)?;
+    // all_circuits.verify_aggregation(&agg_proof1)?;
+    log::info!("Stage 5");
+    // all_circuits.verify_aggregation(&agg_proof2)?;
+
+    log::info!("Stage 6");
+
+
+    // Test retrieved public values from the proof public inputs.
+    // let retrieved_public_values0 = PublicValues::from_public_inputs(&agg_proof0.public_inputs);
+    // assert_eq!(retrieved_public_values0,pv0a);
+    // let retrieved_public_values1 = PublicValues::from_public_inputs(&agg_proof1.public_inputs);
+    // assert_eq!(retrieved_public_values1,pv1);
+    // let retrieved_public_values2 = PublicValues::from_public_inputs(&agg_proof2.public_inputs);
+    // assert_eq!(retrieved_public_values2,pv2);
+
+    log::info!("Stage 7a");
+    // assert_eq!(pv0a.trie_roots_before.state_root,pv0a.extra_block_data.checkpoint_state_trie_root);
+
+    // let (block_proof0, _block_public_values) = all_circuits.prove_block(
+    //     None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
+    //     &agg_proof0,
+    //     pv0a.clone(),
+    // )?;
+    log::info!("Stage 7b");
+    // let (block_proof1, _block_public_values) = all_circuits.prove_block(
+    //     None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
+    //     &agg_proof1,
+    //     pv1.clone(),
+    // )?;
+    // log::info!("Stage 7c");
+    // let (block_proof2, _block_public_values) = all_circuits.prove_block(
+    //     None, // We don't specify a previous proof, considering block 1 as the new checkpoint.
+    //     &agg_proof2,
+    //     pv2.clone(),
+    // )?;
+    let block_proof4 = generate_test_block_proof(123, &mut timing, &all_circuits, &all_stark, config)?;
+    log::info!("Stage 8");
     // Everything above this point should be refactored to a function.
+    // all_circuits.verify_block(&block_proof0)?;
+    // all_circuits.verify_block(&block_proof1)?;
+    // all_circuits.verify_block(&block_proof2)?;
+    all_circuits.verify_block(&block_proof4)?;
 
-    let proof01 = all_circuits.prove_two_to_one_block_einar(&block_proof0, &block_proof1, pv0.clone(), pv1)?;
-    all_circuits.verify_two_to_one_block_einar(&proof01)?;
+    log::info!("Stage 9");
+    //let proof01 = all_circuits.prove_two_to_one_block_einar(&block_proof0, &block_proof1, pv0.clone(), pv1)?;
+    // all_circuits.verify_two_to_one_block_einar(&proof01)?;
 
-    let pv01 = pv0; // + pv1;
-    let proof012 = all_circuits.prove_two_to_one_block_einar(&proof01, &agg_proof2, pv01, pv2)?;
-    all_circuits.verify_two_to_one_block_einar(&proof012)
+    // let pv01 = pv0; // + pv1;
+    // let proof012 = all_circuits.prove_two_to_one_block_einar(&proof01, &agg_proof2, pv01, pv2)?;
+    // all_circuits.verify_two_to_one_block_einar(&proof012)
+    Ok(())
 }
 
 fn eth_to_wei(eth: U256) -> U256 {
